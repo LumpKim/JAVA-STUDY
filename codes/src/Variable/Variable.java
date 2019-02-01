9308c0764401ac433a05223b39130cb1f8cbcff0
by09115 <by09115@outlook.kr>
2019-02-01 10:59:19 +0900

    [add] 변수 선언과 산술 연산에 관한 예제 작성

---
+++ codes/src/Variable/Variable.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class Variable {
+    public static void main(String[] args) {
+        // 정수형 변수 value 를 10으로 초기화
+        int value = 10;
+
+        // 정수형 변수 result 에 변수 value 와 10을 더한 값을 초기화
+       int result = value + 10;
+
+       // 변수 result 의 값을 출력
+        System.out.println(result);
+    }
+}
