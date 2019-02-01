258cd5a83080633bcc5a107166e02a05f372beef
by09115 <by09115@outlook.kr>
2019-02-01 10:51:31 +0900

    [edit] 주석 추가

--- codes/src/Hello/HelloWorld.java
+++ codes/src/Hello/HelloWorld.java
@@ -1,7 +1,9 @@
 package Hello;
 
 public class HelloWorld {
+    // 프로그램 실행 진입
     public static void main(String[] args) {
+        // 콘솔에 "Hello, World!" 출력
         System.out.println("Hello, World!");
     }
 }
