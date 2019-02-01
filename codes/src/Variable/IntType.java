421e416316d437eb18bd6e81d9c1974bb4180ada
by09115 <by09115@outlook.kr>
2019-02-01 12:01:28 +0900

    [add] int 형 변수에 관한 예제 작성

---
+++ codes/src/Variable/IntType.java
@@ -0,0 +1,13 @@
+package Variable;
+
+public class IntType {
+    public static void main(String[] args) {
+        int temp = 10;      // 10진수로 초기화
+        int temp2 = 012;    // 8진수로 초기화
+        int temp3 = 0xA;    // 16진수로 초기화
+
+        System.out.println(temp);
+        System.out.println(temp2);
+        System.out.println(temp3);
+    }
+}
