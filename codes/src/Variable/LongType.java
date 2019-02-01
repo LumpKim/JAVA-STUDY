df381fbb6f01cae1b993de856dcd6a2854509176
by09115 <by09115@outlook.kr>
2019-02-01 12:01:44 +0900

    [add] long 형 변수에 관한 예제 작성

---
+++ codes/src/Variable/LongType.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class LongType {
+    public static void main(String[] args) {
+        long temp = 10;
+        long temp2 = 100L;
+        // long temp3 = 1000000000000;  에러 발생
+        long temp4 = 1000000000000L;
+
+        System.out.println(temp);
+        System.out.println(temp2);
+        System.out.println(temp4);
+    }
+}
