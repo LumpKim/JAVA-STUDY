93ed5f86a3c4892c30a7b276886ef51d1bcfa0ae
by09115 <by09115@outlook.kr>
2019-02-01 10:50:57 +0900

    [add] 변수 선언 관련 예제 작성

---
+++ codes/src/ExecuteStatement/ExecuteStatement.java
@@ -0,0 +1,11 @@
+package ExecuteStatement;
+
+public class ExecuteStatement {
+    // 실행문과 세미콜론 연습: 두 개의 변수를 초기화하고, 둘의 합을 출력하기
+    public static void main(String[] args) {
+        int x = 1;  // 정수형 변수 x에 1 대입
+        int y = 2;  // 정수형 변수 y에 2 대입
+        int result = x + y; // 정수형 변수 result 에 x와 y의 합을 대입
+        System.out.println(x + "+" + y + "=" + result); // 연산 결과 출력
+    }
+}
